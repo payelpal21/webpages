a39180bf4f178b8f66cd4b8b30b00c83b3d74bbe
Payel Pal <palpayel212002@gmail.com>
2024-07-27 22:15:58 +0530

    done a small work

--- BMI calculator/script.js
+++ BMI calculator/script.js
@@ -11,6 +11,7 @@ e.preventDefault();//default action thamano hocche
 const height=parseInt(document.querySelector('#height').value)//parseInt int e convert korbe
 const weight=parseInt(document.querySelector('#weight').value)
 const results = document.querySelector('#results');
+const resultsbox = document.querySelector('#resultbox');
 
 if(height===''|| height<=0 || isNaN(height)){
     results.innerHTML="Please give a valid height"
@@ -19,6 +20,19 @@ if(height===''|| height<=0 || isNaN(height)){
 }else{
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
     //show the result
-    results.innerHTML = `<span>${bmi}</span>`;
+   //addind extra as assignment
+    if(bmi<18.6){
+        resultsbox.innerHTML = `<span>weight = ${bmi}</span>`;
+        results.innerHTML = (`your weight is under weight as you have ${bmi}`);
+    }
+    else if(bmi=>18.6||bmi<=24.9){
+        resultsbox.innerHTML = `<span>weight = ${bmi}</span>`;
+        results.innerHTML = (`your weight is normal as you have ${bmi}`);
+    }
+    else{
+        resultsbox.innerHTML = `<span>weight = ${bmi}</span>`;
+        results.innerHTML = (`your weight is over weight as you have ${bmi}`);
+    }
 }
+
 })
