88daf3fed8544c94ededf6879aa9706091f7edbc
Payel Pal <palpayel212002@gmail.com>
2025-04-09 00:17:00 +0530

    Gallery with horizontal scrolling

---
+++ Gallery/script.js
@@ -0,0 +1,19 @@
+let scrollContainer = document.querySelector(".gallery");
+let backBtn = document.getElementById("backBtn");
+let nextBtn = document.getElementById("nextBtn");
+
+scrollContainer.addEventListener("wheel", (evt) => {
+  evt.preventDefault();
+  scrollContainer.scrollLeft += evt.deltaY;
+  scrollContainer.style.scrollBehavior = "auto";
+});
+
+nextBtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft += 960; // scroll 3 image widths
+});
+
+backBtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft -= 960;
+});
